668dc067b628e8ecda2bb23c9ffda0379dc6a1f5
정채린 <rosachae12@gmail.com>
2024-05-14 17:23:56 +0900

    환경 파일 추가

--- src/router/index.js
+++ src/router/index.js
@@ -8,23 +8,6 @@ import { storeToRefs } from "pinia";
 
 import { useMemberStore } from "@/stores/member";
 
-// const onlyAuthUser = async (to, from, next) => {
-//   const memberStore = useMemberStore();
-//   const { userInfo, isValidToken } = storeToRefs(memberStore);
-//   const { getUserInfo } = memberStore;
-
-//   let token = sessionStorage.getItem("accessToken");
-
-//   if (userInfo.value != null && token) {
-//     await getUserInfo(token);
-//   }
-//   if (!isValidToken.value || userInfo.value === null) {
-//     next({ name: "user-login" });
-//   } else {
-//     next();
-//   }
-// };
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -53,34 +36,6 @@ const router = createRouter({
         },
       ],
     },
-    // {
-    //   path: "/user",
-    //   name: "user",
-    //   component: () => import("@/views/TheUserView.vue"),
-    //   children: [
-    //     {
-    //       path: "login",
-    //       name: "user-login",
-    //       component: () => import("@/components/users/UserLogin.vue"),
-    //     },
-    //     {
-    //       path: "join",
-    //       name: "user-join",
-    //       component: () => import("@/components/users/UserRegister.vue"),
-    //     },
-    //     {
-    //       path: "mypage",
-    //       name: "user-mypage",
-    //       beforeEnter: onlyAuthUser,
-    //       component: () => import("@/components/users/UserMyPage.vue"),
-    //     },
-    //   ],
-    // },
-    // {
-    //   path: "/todos",
-    //   name: "todos",
-    //   component: () => import("@/views/TheTodoView.vue"),
-    // },
   ],
 });
 

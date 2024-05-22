819846036b71062b10c976bfaee259cf99a6b559
chaerin-77 <rosachae12@gmail.com>
2024-05-23 01:03:27 +0900

    메인화면 수정

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, computed } from "vue";
+import { ref, computed, onMounted } from "vue";
 import { RouterLink } from "vue-router";
 import { useAuthStore } from "@/stores/auth";
 import { useGroupStore } from "@/stores/group";
@@ -12,14 +12,38 @@ const authStore = useAuthStore();
 const user = computed(() => authStore.user);
 const groupStore = useGroupStore();
 const groupList = computed(() => groupStore.groupList);
+
+const typedText = ref("");
+const txt = "모여서 여행?\n모이면 행복!";
+let i = 0;
+
+const viewText = () => {
+  if (i < txt.length) {
+    if (txt.charAt(i) === "\n") {
+      typedText.value += "<br>";
+    } else {
+      typedText.value += txt.charAt(i);
+    }
+    i++;
+    setTimeout(viewText, 300);
+  }
+};
+
+onMounted(() => {
+  viewText();
+});
 </script>
 
 <template>
   <div class="bg-main-color p-20">
     <div class="container flex justify-between">
-      <div class="w-3/5">
-        <h1 class="text-white text-xl font-semibold mb-6">내 프로필</h1>
-        <div class="bg-white rounded-2xl p-5 flex justify-between">
+      <div
+        v-html="typedText"
+        class="text-white font-bold text-8xl p-5"
+        style="line-height: 150%"
+      ></div>
+      <div class="w-2/5 bg-white shadow-lg rounded-2xl p-5">
+        <div class="flex justify-between">
           <div
             class="h-28 w-28"
             :style="`background-image: url(${authStore.user.img}); background-size: cover; background-position: center;`"
@@ -39,38 +63,34 @@ const groupList = computed(() => groupStore.groupList);
             </div>
           </div>
         </div>
-        <div class="bg-gray-500 p-4 text-center rounded-xl">
-          <RouterLink :to="{ name: 'myinfo' }" class="hover: no-underline"
-            ><p class="text-white">내 정보 수정하기 ></p></RouterLink
-          >
-        </div>
-      </div>
-      <div class="ml-10 w-full">
-        <h1 class="text-white text-xl font-semibold mb-6">내 여행 모임</h1>
-        <div class="flex p-5 border-2 border-white rounded-2xl flex-wrap">
-          <div
-            v-for="group in groupList"
-            :key="group.groupId"
-            class="text-center mr-3"
-          >
+        <hr class="mt-4" />
+        <div class="w-full mt-4">
+          <h1 class="text-gray-600 text-lg font-semibold mb-3">내 여행 모임</h1>
+          <div class="flex p-2 border-2 border-white rounded-2xl flex-wrap">
             <div
-              class="rounded-full border-2 border-white w-20 h-20"
-              :style="`background-image: url(${group.groupImg}); background-size: cover; background-position: center;`"
-            ></div>
-            <p class="text-white text-md mt-3">{{ group.groupName }}</p>
-          </div>
-          <div class="text-center">
-            <RouterLink :to="{ name: 'invite' }">
+              v-for="group in groupList"
+              :key="group.groupId"
+              class="text-center mr-3"
+            >
               <div
-                class="hover:shadow-xl rounded-full bg-orange-300 w-20 h-20 pt-4"
-              >
-                <font-awesome-icon
-                  icon="plus"
-                  style="color: white"
-                  size="2xl"
-                />
-              </div>
-            </RouterLink>
+                class="rounded-full border-2 border-gray-600 w-14 h-14"
+                :style="`background-image: url(${group.groupImg}); background-size: cover; background-position: center;`"
+              ></div>
+              <p class="text-gray-600 text-md mt-3">{{ group.groupName }}</p>
+            </div>
+            <div class="text-center">
+              <RouterLink :to="{ name: 'invite' }">
+                <div
+                  class="hover:shadow-xl rounded-full bg-orange-300 w-14 h-14 pt-3"
+                >
+                  <font-awesome-icon
+                    icon="plus"
+                    style="color: white"
+                    size="xl"
+                  />
+                </div>
+              </RouterLink>
+            </div>
           </div>
         </div>
       </div>

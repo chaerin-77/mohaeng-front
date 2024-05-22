0bb0753f6ee5e6575e11de793371ebcb5d1525e9
chaerin-77 <rosachae12@gmail.com>
2024-05-22 20:16:17 +0900

    api

--- src/api/todoApi.js
+++ src/api/todoApi.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const todoApi = axios.create({
-  baseURL: "http://localhost:9000/api/todos", // 내 컴퓨터
+  // baseURL: "http://localhost:9000/api/todos", // 내 컴퓨터
   // baseURL: "http://192.168.206.61:9000/api/todos", // ssafy 컴퓨터
+  baseURL: "http://172.30.1.38:9000/api/todos", // 언니 컴퓨터
 });
 
 export default todoApi;

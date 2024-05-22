12a4df43f5dbcd640294454fe5349705e8328d8f
김민경 <wogml5890@gmail.com>
2024-05-22 14:58:06 +0900

    chatbot

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,26 @@ import { RouterView, useRoute } from "vue-router";
 import { ref, watchEffect } from "vue";
 import Navigation from "./components/Navigation.vue";
 import Footer from "./components/Footer.vue";
+import ChatBot from "@/components/chatbot/ChatBot.vue";
 
 const showNavbar = ref(true);
 const route = useRoute();
+const isChatOpen = ref(false);
 
+// 외부 클릭 감지를 위한 함수
+const handleClickOutside = (event) => {
+  const chatbotElement = document.querySelector(".chatbot");
+  const chatbotIcon = document.querySelector(".chatbot-icon");
+  if (
+    isChatOpen.value &&
+    chatbotElement &&
+    !chatbotElement.contains(event.target) &&
+    !chatbotIcon.contains(event.target)
+  ) {
+    isChatOpen.value = false;
+  }
+};
+document.addEventListener("click", handleClickOutside);
 watchEffect(() => {
   // 특정 페이지에 대한 meta 정보 확인
   showNavbar.value = route.meta.showNavbar; // 네비게이션 바를 숨김
@@ -20,6 +36,11 @@ watchEffect(() => {
     </div>
     <router-view></router-view>
     <div v-if="showNavbar">
+      <ChatBot
+        :isChatOpen="isChatOpen"
+        @toggleChat="isChatOpen = !isChatOpen"
+        @openChat="isChatOpen = true"
+      />
       <Footer />
     </div>
   </div>
